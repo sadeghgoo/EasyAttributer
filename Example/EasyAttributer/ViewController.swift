a69a6386f94fdfc481b87cbc6e3a7bdade30b0a9
Sadegh <sadeghitunes2@gmail.com>
2021-10-06 09:43:53 +0330

    Initial commit

---
+++ Example/EasyAttributer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  EasyAttributer
+//
+//  Created by Sadegh on 10/06/2021.
+//  Copyright (c) 2021 Sadegh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
